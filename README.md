af7b9ad8e3d93081a0f4e9026dcc134310d434e9
Yu-Hua Chang <cyrax@ms25.hinet.net>
2016-09-30 20:37:34 +0800

    init commit

---
+++ README.md
@@ -0,0 +1,4 @@
+SpringBoot + Spring Security + AzureAD (Office 365)
+===================================================
+
+This sample demonstrates a SpringBoot application with Spring Security using Azure AD (provided by Office 365) to authenticate static web and web services.
---
+++ src/main/java/com/example/AzureadOauthApplication.java
@@ -0,0 +1,12 @@
+package com.example;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AzureadOauthApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AzureadOauthApplication.class, args);
+	}
+}
